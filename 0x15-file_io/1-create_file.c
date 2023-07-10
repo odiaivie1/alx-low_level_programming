4639c8fa1d986a03a2c63714fcb9e51ddb8fcc9c
odiaivie1 <ivieodia0@gmail.com>
2023-07-10 06:06:12 -0700

    Task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h#"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: Pointer that point to the file name
+ * @text_content: A pointer to a string
+ * Return: --1 (fail), else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int pd, m, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; tect_content[length];)
+			length++;
+	}
+
+	pd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	m = write(pd, text_content, length);
+
+	if (pd == -1 || m == -1)
+		return (-1);
+
+	close(pd);
+
+	return (1);
+}
