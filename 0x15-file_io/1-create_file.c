60add583cc2232f076c9c8340dbab132d9ac53e7
odiaivie1 <ivieodia0@gmail.com>
2023-07-10 06:08:50 -0700

    Task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (length = 0; tect_content[length];)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 
